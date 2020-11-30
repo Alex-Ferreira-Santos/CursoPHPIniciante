4892c8e13227ea47f9e45a1588436b667a54649d
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2020-11-30 09:38:58 -0300

    aula 3 e 4

---
+++ aula3/ola.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo PHP</title>
+    <style>
+        h2{
+            color: #80a2ff;
+            text-shadow: 1px 1px 1px rgba(255, 255, 255)
+        }
+    </style>
+</head>
+<body>
+    <h1>testando php</h1>
+    <?php
+        echo "<h2>Olá, mundo!</h2>"
+        
+    ?>
+</body>
+</html>
+
